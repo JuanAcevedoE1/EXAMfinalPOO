9e9b766451e39ec95753a3a4a2093ecb903d2326
JuanAcevedoE1 <jacevedoe1@unicartagena.edu.co>
2024-12-14 16:50:58 -0500

    Creacion y actualizacion GuiEditorial, creacion de GestorEditorial, arreglo de pequeños errores

--- src/edu/unicartagena/poo/gui/GuiEditorial.java
+++ src/edu/unicartagena/poo/gui/GuiEditorial.java
@@ -35,11 +35,11 @@ public class GuiEditorial extends javax.swing.JDialog {
         jTextField2 = new javax.swing.JTextField();
         jTextField3 = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
+        BotonGuardar = new javax.swing.JButton();
+        BotonCancelar = new javax.swing.JButton();
+        BotonEliminar = new javax.swing.JButton();
+        BotonEditar = new javax.swing.JButton();
+        BotonBuscar = new javax.swing.JButton();
         jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
@@ -102,20 +102,20 @@ public class GuiEditorial extends javax.swing.JDialog {
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("FORMULARIO EDITORIAL");
 
-        jButton1.setFont(new java.awt.Font("Arial", 3, 15)); // NOI18N
-        jButton1.setText("GUARDAR");
+        BotonGuardar.setFont(new java.awt.Font("Arial", 3, 15)); // NOI18N
+        BotonGuardar.setText("GUARDAR");
 
-        jButton2.setFont(new java.awt.Font("Arial", 3, 15)); // NOI18N
-        jButton2.setText("CANCELAR");
+        BotonCancelar.setFont(new java.awt.Font("Arial", 3, 15)); // NOI18N
+        BotonCancelar.setText("CANCELAR");
 
-        jButton3.setFont(new java.awt.Font("Arial", 3, 15)); // NOI18N
-        jButton3.setText("ELIMINAR");
+        BotonEliminar.setFont(new java.awt.Font("Arial", 3, 15)); // NOI18N
+        BotonEliminar.setText("ELIMINAR");
 
-        jButton4.setFont(new java.awt.Font("Arial", 3, 15)); // NOI18N
-        jButton4.setText("EDITAR");
+        BotonEditar.setFont(new java.awt.Font("Arial", 3, 15)); // NOI18N
+        BotonEditar.setText("EDITAR");
 
-        jButton5.setFont(new java.awt.Font("Arial", 3, 15)); // NOI18N
-        jButton5.setText("BUSCAR");
+        BotonBuscar.setFont(new java.awt.Font("Arial", 3, 15)); // NOI18N
+        BotonBuscar.setText("BUSCAR");
 
         jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/edu/unicartagena/poo/gui/iconos/pencil-icon.png"))); // NOI18N
 
@@ -125,15 +125,15 @@ public class GuiEditorial extends javax.swing.JDialog {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(169, 169, 169)
-                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(BotonEditar, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(BotonEliminar, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(31, 31, 31)
-                .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(BotonBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(BotonCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(59, 59, 59)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(BotonGuardar, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(65, 65, 65))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(15, 15, 15)
@@ -157,12 +157,12 @@ public class GuiEditorial extends javax.swing.JDialog {
                     .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 285, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(48, 48, 48)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(BotonCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(BotonGuardar, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(BotonEliminar, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(BotonEditar, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(BotonBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
         );
 
@@ -212,11 +212,11 @@ public class GuiEditorial extends javax.swing.JDialog {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
+    private javax.swing.JButton BotonBuscar;
+    private javax.swing.JButton BotonCancelar;
+    private javax.swing.JButton BotonEditar;
+    private javax.swing.JButton BotonEliminar;
+    private javax.swing.JButton BotonGuardar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
